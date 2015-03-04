28aa89d7fd7f554c230cbe4de97d8e6f55e37032
Marc <marc.roedder@finanzen100.de>
2015-03-04 23:13:37 +0100

    Initial

---
+++ SwiftyMapper/SwiftyMapper.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyMapper.h
+//  SwiftyMapper
+//
+//  Created by sticksen  on 04.03.15.
+//  Copyright (c) 2015 sticksen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyMapper.
+FOUNDATION_EXPORT double SwiftyMapperVersionNumber;
+
+//! Project version string for SwiftyMapper.
+FOUNDATION_EXPORT const unsigned char SwiftyMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyMapper/PublicHeader.h>
+
+
---
+++ SwiftyMapper/SwiftyMapper.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftyMapper.swift
+//  SwiftyMapper
+//
+//  Created by sticksen  on 04.03.15.
+//  Copyright (c) 2015 sticksen. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+import SwiftyJSON
+
+extension Mapper {
+    
+    func map(swiftyJSON json: SwiftyJSON.JSON) -> N? {
+        if let dict = json.dictionaryObject {
+            return map(dict)
+        }
+        return nil
+    }
+}
